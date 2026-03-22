e8e1077f1b0c983d0af5ad70f68024aad609c638
NicabarNimble <nicabar@gmail.com>
2026-03-22 15:12:37 -0400

    core: remove unused daemon client module

--- src/mother/mod.rs
+++ src/mother/mod.rs
@@ -27,7 +27,6 @@
 
 pub mod broker;
 pub(crate) mod checkpoint;
-mod daemon_client;
 pub(crate) mod events;
 mod internal;
 pub(crate) mod state;
@@ -54,7 +53,6 @@ pub use crate::beliefs::{
 };
 
 // Client exports
-pub use daemon_client::DaemonClient;
 pub use internal::{Client, ScryRequest, ScryResponse, ScryResultJson};
 
 /// Default port for mother daemon
