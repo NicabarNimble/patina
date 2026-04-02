9ef2518038562efad10355d41fcd5f5591789200
NicabarNimble <nicabar@gmail.com>
2026-04-01 21:09:30 -0400

    fix: ungating env_test_mutex for integration test visibility

--- src/test_support.rs
+++ src/test_support.rs
@@ -1,7 +1,5 @@
-#[cfg(test)]
 use std::sync::{Mutex, OnceLock};
 
-#[cfg(test)]
 pub fn env_test_mutex() -> &'static Mutex<()> {
     static LOCK: OnceLock<Mutex<()>> = OnceLock::new();
     LOCK.get_or_init(|| Mutex::new(()))
